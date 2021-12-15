6132ea5928e2c5c2357a13c10370578e702b54c5
alskfl222 <alskfl222@kakao.com>
2021-12-15 14:20:38 +0900

    Chore: 서버 환경 세팅

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const https = require('https')
+const router = require('./routes')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('HELLO WORLD');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+module.exports = app;
