ce673338e123e4cd083a6403aa224d9532cc2aa2
alskfl222 <alskfl222@kakao.com>
2021-12-16 14:40:17 +0900

    Chore: CORS 세팅

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const https = require('https');
 const router = require('./routes');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 80;
+
+app.use(
+  cors({
+    origin: "http://book-log-client.s3-website.ap-northeast-2.amazonaws.com/",
+    methods: ['GET', 'POST', 'OPTIONS', 'PATCH'],
+    credentials: true
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('HELLO WORLD');
