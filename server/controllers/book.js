18571f09d7e321b1a57f66aec4725024c1d455f5
kimdoyeonn <doyeon311@gmail.com>
2021-12-16 21:22:55 +0900

    Feat: 도서 리스트 조회

--- server/controllers/book.js
+++ server/controllers/book.js
@@ -1,6 +1,20 @@
-const { book } = require('../models/book');
+const { book } = require('../models');
+const { isAuthorized } = require('./tokenFunctions');
 
 module.exports = {
-  list: () => {},
-  reviews: () => {},
+  list: (req, res) => {
+    const data = isAuthorized(req);
+    if (!data) {
+      res.status(401).json({ message: 'Invalid user' });
+    } else {
+      book.list(data.id, (error, bookList) => {
+        if (error) {
+          res.status(500).json({ message: 'Server Error' });
+        } else {
+          res.json({ message: 'OK', data: { book_list: bookList } });
+        }
+      });
+    }
+  },
+  reviews: (req, res) => {},
 };
