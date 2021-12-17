0c06b6aa01001fdc3d99db3504c94866bb705b7e
kimdoyeonn <doyeon311@gmail.com>
2021-12-17 14:30:47 +0900

    Feat: 감상삭제

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -1,7 +1,21 @@
-const { review } = require('../models/review');
+const { review } = require('../models');
+const { isAuthorized } = require('./tokenFunctions');
 
 module.exports = {
   new: () => {},
   edit: () => {},
-  remove: () => {},
+  remove: (req, res) => {
+    const data = isAuthorized(req);
+    if (!data) {
+      res.status(500).json({ message: 'Server Error' });
+    } else {
+      const { review_id } = req.params;
+      review.remove(review_id, (error, result) => {
+        if (error) {
+          res.status(500).json({ message: 'Server Error' });
+        }
+        res.json({ message: 'ok' });
+      });
+    }
+  },
 };
--- server/models/review.js
+++ server/models/review.js
@@ -3,5 +3,14 @@ const db = require('../db');
 module.exports = {
   new: () => {},
   edit: () => {},
-  remove: () => {},
+  remove: (reviewId, callback) => {
+    const queryString = `
+      DELETE FROM review WHERE id = ?
+    `;
+    db.query(queryString, [reviewId], (error, result) => {
+      if (error) throw error;
+
+      callback(error, result);
+    });
+  },
 };
