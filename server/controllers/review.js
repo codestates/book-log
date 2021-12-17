f0a29614153dd7024ce63cd5805e70d0a8612373
kimdoyeonn <doyeon311@gmail.com>
2021-12-17 13:56:24 +0900

    Feat: 감상 수정

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -1,7 +1,21 @@
-const { review } = require('../models/review');
+const { review } = require('../models');
+const { isAuthorized } = require('./tokenFunctions');
 
 module.exports = {
   new: () => {},
-  edit: () => {},
+  edit: (req, res) => {
+    const data = isAuthorized(req);
+    if (!data) {
+      res.json({ message: 'Invalid user' });
+    } else {
+      const { review_id, review: reviewContent } = req.body;
+      review.edit(review_id, reviewContent, (error, result) => {
+        if (error || result.changeRows === 0) {
+          res.status(500).json({ message: 'Server Error' });
+        }
+        res.json({ message: 'ok', data: { review_id } });
+      });
+    }
+  },
   remove: () => {},
 };
--- server/models/review.js
+++ server/models/review.js
@@ -2,6 +2,16 @@ const db = require('../db');
 
 module.exports = {
   new: () => {},
-  edit: () => {},
+  edit: (review_id, review_content, callback) => {
+    const updateQuery = `
+      UPDATE review 
+      SET contents = ?, modified_at = curdate()
+      WHERE id = ?
+    `;
+    db.query(updateQuery, [review_content, review_id], (error, result) => {
+      if (error) throw error;
+      callback(error, result);
+    });
+  },
   remove: () => {},
 };
