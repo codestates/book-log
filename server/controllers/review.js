30311684ecd718ce137ee4f381910c2edf4f98ff
kimdoyeonn <doyeon311@gmail.com>
2021-12-17 12:56:05 +0900

    Feat: 감상작성-책넣기

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -1,7 +1,38 @@
-const { review } = require('../models/review');
+const { review } = require('../models');
 
 module.exports = {
-  new: () => {},
+  new: (req, res) => {
+    const {
+      title,
+      contents,
+      isbn,
+      thumbnail,
+      author,
+      published_at,
+      publisher,
+      url,
+      review: review_contents,
+      page,
+    } = req.body;
+    const bookData = {
+      title,
+      contents,
+      isbn,
+      thumbnail,
+      author,
+      published_at,
+      publisher,
+      url,
+    };
+    const reviewData = {
+      review_contents,
+      page,
+    };
+    review.new(bookData, reviewData, (error, result) => {
+      if (error) throw error;
+      res.json(result);
+    });
+  },
   edit: () => {},
   remove: () => {},
 };
--- server/models/review.js
+++ server/models/review.js
@@ -1,7 +1,50 @@
 const db = require('../db');
 
 module.exports = {
-  new: () => {},
+  new: (bookData, reviewData, callback) => {
+    const {
+      title,
+      contents,
+      isbn,
+      thumbnail,
+      author,
+      published_at,
+      publisher,
+      url,
+    } = bookData;
+    const bookQuery = `
+      SELECT id FROM book WHERE title = ? AND author = ? AND publisher = ?
+      `;
+    db.query(bookQuery, [title, author, publisher], (error, result) => {
+      if (error) throw error;
+      let book_id;
+      if (result.length === 0) {
+        const bookInsert = `
+          INSERT INTO book (title, contents, isbn, thumbnail, author, published_at, publisher, url, created_at)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, curdate())
+        `;
+        db.query(
+          bookInsert,
+          [
+            title,
+            contents,
+            isbn,
+            thumbnail,
+            author,
+            published_at,
+            publisher,
+            url,
+          ],
+          (error, result) => {
+            if (error) throw error;
+            book_id = result.insertId;
+          }
+        );
+      } else {
+        book_id = result[0].id;
+      }
+    });
+  },
   edit: () => {},
   remove: () => {},
 };
