858824a1dd82558885bfea880eaf0652fff01c91
boraborayoon <boraborayoon@hotmail.com>
2021-12-19 23:37:56 +0900

    Feat: ReviewInput 컴포넌트,페이지 작성

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import MdfPassPage from './pages/MdfPassPage';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import BookListPage from './pages/BookListPage';
 import ReviewListPage from './pages/ReviewListPage';
+import ReviewInputPage from './pages/ReviewInputPage';
 
 function App() {
   const [isLogin, setIsLogin] = useState(true);
@@ -69,6 +70,7 @@ function App() {
             path="/booklist/reviewlist"
             element={<ReviewListPage />}
           ></Route>
+          <Route path="/reviewinput" element={<ReviewInputPage />}></Route>
         </Routes>
       </Router>
     </div>
